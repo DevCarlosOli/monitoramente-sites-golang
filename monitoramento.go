4772a835e3edfd1ce1e4ea8270cb356502e88e0b
Carlos Oliveira <c.oliveiradev@gmail.com>
2025-09-29 22:17:07 -0300

    ✨ feat: Upload projects monitoring websites

---
+++ monitoramento.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main()  {
+	for {
+		leSitesDoArquivo()
+		exibeMenu()
+		comando := leComando()
+
+		switch comando {
+			case 1:
+				iniciarMonitoramento()
+			case 2:
+				fmt.Println("Exibindo logs...")
+			case 0:
+				fmt.Println("Saindo do programa...")
+				os.Exit(0)
+			default:
+				fmt.Println("Não conheço este comando...")
+				os.Exit(-1)
+		}
+	}	
+}
+
+func exibeMenu()  {
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O valor da variável comando é:", comandoLido)
+
+	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	sites := leSitesDoArquivo()
+	
+	for i, site := range sites {
+		fmt.Println("Testando site", i, ":", site)
+		testaSite(site)
+	}
+
+	fmt.Println("")
+}
+
+func testaSite(site string) {
+	resp, err := http.Get(site)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
+}
+
+func leSitesDoArquivo() []string  {
+	var sites []string
+	arquivo, err := os.Open("sites.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro", err)
+	}
+
+	leitor := bufio.NewReader(arquivo)
+
+	for {
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+		
+		sites = append(sites, linha)
+		
+		if err == io.EOF {
+			break		
+		}
+
+	}
+
+	arquivo.Close()
+
+	return sites
+}
